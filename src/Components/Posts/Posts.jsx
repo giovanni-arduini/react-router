2d342453d8df54e0b58b05eaf95248dc4d092e88
giovanni-arduini <gionnibigudd@gmail.com>
2024-12-06 15:06:21 +0100

    ADD Posts components

---
+++ src/Components/Posts/Posts.jsx
@@ -0,0 +1,13 @@
+function Posts() {
+  return (
+    <>
+      <h3>Your Posts</h3>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio
+        rerum soluta aspernatur accusamus. Qui, deserunt.
+      </p>
+    </>
+  );
+}
+
+export default Posts;

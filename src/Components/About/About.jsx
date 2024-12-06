f7485e3edb6803dcb9cdb34b1c9b66be448b2c64
giovanni-arduini <gionnibigudd@gmail.com>
2024-12-06 15:05:51 +0100

    ADD About Component

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,13 @@
+function About() {
+  return (
+    <>
+      <h3>About us</h3>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio
+        rerum soluta aspernatur accusamus. Qui, deserunt.
+      </p>
+    </>
+  );
+}
+
+export default About;

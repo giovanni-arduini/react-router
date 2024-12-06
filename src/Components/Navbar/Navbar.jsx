01016190dc4de91f1128b7fa37fdf3bf154ea0e5
giovanni-arduini <gionnibigudd@gmail.com>
2024-12-06 14:53:16 +0100

    ADD Navbar Component

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <ul>
+      <li>
+        <NavLink to="">About</NavLink>
+      </li>
+      <li>
+        <NavLink to="">Contacts</NavLink>
+      </li>
+      <li>
+        <NavLink to="">Posts</NavLink>
+      </li>
+    </ul>
+  );
+}

f320d2399855d8884689d466b4e5af87627152df
giovanni-arduini <gionnibigudd@gmail.com>
2024-12-06 15:05:59 +0100

    ADD

---
+++ src/Components/Contacts/Contacts.jsx
@@ -0,0 +1,12 @@
+function Contacts() {
+  return (
+    <>
+      <h3>Contacts</h3>
+      <p>Lorem ipsum dolor sit amet.</p>{" "}
+      <p>Lorem ipsum dolor sit amet consectetur.</p>
+      <p>Lorem ipsum dolor sit amet.</p>
+    </>
+  );
+}
+
+export default Contacts;

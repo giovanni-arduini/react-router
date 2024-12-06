421f9a6c15d4b91267e62a52dacdb804bc495fad
giovanni-arduini <gionnibigudd@gmail.com>
2024-12-06 15:05:35 +0100

    ADD routing to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
+import About from "./Components/About/About";
+import Posts from "./Components/Posts/Posts";
+import Contacts from "./Components/Contacts/Contacts";
 
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route index element={<Posts />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/contacts" element={<Contacts />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;

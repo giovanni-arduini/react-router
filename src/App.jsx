dc9471cd05caad8f84ca9ca64ed2cf061152b35b
giovanni-arduini <gionnibigudd@gmail.com>
2024-12-10 15:43:47 +0100

    ADD nested routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./Components/Navbar/Navbar";
-import About from "./Components/About/About";
-import Home from "./Components/Home/Home";
-import Posts from "./Components/Posts/Posts";
+import Navbar from "./Components/Navbar";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Show from "./pages/posts/Show";
+import Index from "./pages/posts/Index";
+import Create from "./pages/posts/Create";
 
 import "./App.css";
+import ErrorNotFound from "./pages/ErrorNotFound/ErrorNotFound";
+
+const API_BASE_URI = "http://localhost:3000/";
 
 function App() {
   return (
@@ -13,9 +17,20 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route index element={<Home />}></Route>
-          <Route path="/about" element={<About />}></Route>
-          <Route path="/posts" element={<Posts />}></Route>
+          <Route>
+            <Route index element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+          </Route>
+
+          <Route path="/posts">
+            <Route path="" Component={Index}></Route>
+            <Route path=":id" Component={Show}></Route>
+            <Route path="create" Component={Create}></Route>
+          </Route>
+
+          <Route>
+            <Route path="*" element={<ErrorNotFound />}></Route>
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
